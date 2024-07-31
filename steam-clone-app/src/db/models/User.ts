46eb28e30578779337961411b0d72a8e3510d951
YoelK20 <166104713+YoelK20@users.noreply.github.com>
2024-07-31 11:58:03 +0700

    Login API Setup Done

--- steam-clone-app/src/db/models/User.ts
+++ steam-clone-app/src/db/models/User.ts
@@ -18,13 +18,13 @@ export type UserModelCreateInput = Omit<UserInput, "_id">;
 type UserLoginInput = {
   username: string;
   password: string;
-}
+};
 
 export type UserPayload = {
   id: ObjectId;
   name: string;
   username: string;
-}
+};
 
 const UserSchema = z.object({
   name: z.string({
@@ -47,12 +47,15 @@ const UserSchema = z.object({
     .min(5, { message: "Minimum Password is 5 Characters" }),
 });
 
-const LoginSchema = z.object({ username: z.string({ message: "Please Input your Username" }), password: z.string({ message: "Please Input your Password" }) })
+const LoginSchema = z.object({
+  username: z.string({ message: "Please Input your Username" }),
+  password: z.string({ message: "Please Input your Password" }),
+});
 
 class UserModel {
   static async registerUser(newUser: UserModelCreateInput) {
     UserSchema.parse(newUser);
-    const DB = await getDB()
+    const DB = await getDB();
     const checkMail = await DB.collection("Users").findOne({
       email: newUser.email,
     });
@@ -64,16 +67,40 @@ class UserModel {
     if (checkMail) throw new Error("Email already Exists");
     if (checkUsername) throw new Error("Username already Exists");
 
-    const user : UserModelCreateInput = {
+    const user: UserModelCreateInput = {
       ...newUser,
-      password : hashText(newUser.password)
-    }
+      password: hashText(newUser.password),
+    };
     const result = DB.collection("Users").insertOne(user);
 
     return result;
   }
 
-  
+  static async userLogin(user: UserLoginInput) {
+    LoginSchema.parse(user);
+
+    const DB = await getDB();
+    const findUsername = await DB.collection("Users").findOne({
+      username: user.username,
+    });
+
+    if (!findUsername) throw new Error("Invalid Username or Password");
+
+    const checkPassword = compareTextWithHash(
+      user.password,
+      findUsername.password
+    );
+    if (!checkPassword) throw new Error("Invalid Username or Password");
+
+    const payload: UserPayload = {
+      id: findUsername._id,
+      name: findUsername.name,
+      username: findUsername.username,
+    };
+    const access_token = getToken(payload);
+
+    return { username: findUsername.username, access_token };
+  }
 }
 
 export default UserModel;
